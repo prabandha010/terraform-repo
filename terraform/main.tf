cf9119226019a95fa90a503c8146a191c1082a01
prabandha010 <116628383+prabandha010@users.noreply.github.com>
2022-12-07 16:27:32 +0530

    Update main.tf
    msg

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,7 +4,7 @@ access_key = "AKIA3RQAVD4A5TSAVVEC"
 secret_key = "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"
 }
 
-resource "aws_instance" "web"
+resource "aws_instance" "web" {
 ami = "ami-074dc0a6f6c764218"
 instance_type = "t2.micro"
 tags = {
